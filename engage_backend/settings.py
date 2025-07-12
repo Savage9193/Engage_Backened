a3ddbbb93b7a2354b6f4e4400e39d1d7be18ac07
Savage9193 <tyagishahvez551@gmail.com>
2025-07-12 12:22:34 +0530

    fixed

--- engage_backend/settings.py
+++ engage_backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
