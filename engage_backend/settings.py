ba43211ecc9dbf5da6b1f0a50b7a5c67c85f9530
Savage9193 <tyagishahvez551@gmail.com>
2025-07-12 12:34:40 +0530

    debug true

--- engage_backend/settings.py
+++ engage_backend/settings.py
@@ -37,7 +37,7 @@ EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 SECRET_KEY = "django-insecure-7+2=#qr4fkrn1-=5cqb1)0#gmc4f0v8n&hqw!@l1$6f8y9a9c3"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']  # Use your domain name here if you have one, e.g., 'yourdomain.com'
 
 # OR your render domain when 
